1bc954f360130de11fc8848777c0ca77b949365b
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-07-14 21:27:02 -0700

    fix: correct BASEDIR path and update script to reference the correct Python file location

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-BASEDIR="/home/picam//dht11adaf"
+BASEDIR="/home/picam/dht11adaf"
 
 if [ -n "$VIRTUAL_ENV" ]; then
     echo "Yes, a Python virtual environment is active."
     echo "Venv path: $VIRTUAL_ENV"
     cd $BASEDIR
-    if [ -f "./dht11adaf.py" ]; then
+    if [ -f "./dht11adafruit/dht11adaf.py" ]; then
         echo "Running dht11adaf.py"
-        python3 ./dht11adaf.py
+        python3 ./dht11adafruit/dht11adaf.py
     else
         echo "Error: dht11adaf.py not found in $BASEDIR"
     fi
@@ -17,9 +17,9 @@ else
     echo "Activating Now"
     cd $BASEDIR
     source ./bin/activate
-    if [ -f "./dht11adaf.py" ]; then
+    if [ -f "./dht11adafruit/dht11adaf.py" ]; then
         echo "Running dht11adaf.py"
-        python3 ./dht11adaf.py
+        python3 ./dht11adafruit/dht11adaf.py
     else
         echo "Error: dht11adaf.py not found in $BASEDIR"
     fi
